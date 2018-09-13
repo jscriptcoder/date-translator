a704444472889cb252ea16dd3cbec45f8b6a1094
jscriptcoder <jscriptcoder@gmail.com>
2018-09-13 22:35:32 +0200

    Changed toolchain

---
+++ src/DateTranslator.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function DateTranslator (props) {
+  return <div>DateTranslator</div>
+}
---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DateTranslator from './DateTranslator'
+
+const App = props => (
+  <div className="app">
+    { props.children }
+  </div>
+)
+
+const appNode = document.getElementById('example')
+
+ReactDOM.render(
+  <App>
+    <DateTranslator />
+  </App>,
+  appNode
+)
