a58561246520203560819e7ef16f5096c43214c9
jscriptcoder <jscriptcoder@gmail.com>
2018-09-26 22:55:44 +0200

    Trying to import the model

--- src/DateTranslator.jsx
+++ src/DateTranslator.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import * as React from 'react'
+import * as tf from '@tensorflow/tfjs'
 
-export default function DateTranslator (props) {
-  return <div>DateTranslator</div>
+export default class DateTranslator extends React.Component {
+
+  async componentDidMount() {
+    this.model = await tf.loadModel('./tfjsmodel/model.json');
+  }
+
+  render() {
+    return <div>DateTranslator</div>
+  }
 }
