5a5d802a7658f75b022d735388bde5e25d1ae696
jscriptcoder <jscriptcoder@gmail.com>
2018-10-29 13:41:31 +0100

    Finalizing setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require("path")
+const webpack = require("webpack")
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = env => {
 
-  const isExample = !env
+  const prod = env && env.production
 
   return {
-    mode: isExample ? 'development' : 'production',
-    entry: `./src/${isExample ? 'example.js' : 'DateTranslator.js'}`,
+    mode: prod ? 'production' : 'development',
+    entry: `./src/${prod ? 'DateTranslator.js' : 'example.js'}`,
     output: {
-      path: path.resolve(__dirname, isExample ? 'example/' : 'lib/'),
+      path: path.resolve(__dirname, prod ? 'lib/' : 'example/'),
       filename: 'index.js'
     },
+    externals: prod ? [ nodeExternals() ] : {},
     module: {
       rules: [
         {
@@ -35,9 +37,10 @@ module.exports = env => {
     plugins: [ new webpack.HotModuleReplacementPlugin() ],
     devServer: {
       contentBase: path.join(__dirname, "example/"),
-      port: 3000,
-      hotOnly: true
+      port: 5000,
+      hotOnly: true,
+      open: true,
     },
-    devtool: isExample ? 'source-map' : false,
+    devtool: prod ? false : 'source-map',
   }
 }
